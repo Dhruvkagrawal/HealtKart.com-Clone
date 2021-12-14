e3a743ce3899138f38b0a56f0de34d095b2422a5
Arpit Shrivastava <arpitshrivastava764@gmail.com>
2021-12-14 13:55:07 +0530

    Added Authentication And Google auth

--- src/index.js
+++ src/index.js
@@ -1,4 +1,43 @@
 const express = require("express");
 const app = express();
 
+const { register, login } = require("./controllers/user.controller");
+const passport = require('./configs/passport');
+
+app.use(express.json());
+
+
+app.post("/register", register);
+app.post("/login", login);
+
+app.use(passport.initialize());
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope:
+            ['email', 'profile']
+    }
+    ));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        failureRedirect: '/auth/google/failure'
+}),function (req,res){
+    console.log(req.profile);
+    return res.send(req.user);
+}
+);
+
+
+app.get('/auth/google/failure',function (req,res){
+    return res.send("failed to login");
+})
 module.exports = app;
