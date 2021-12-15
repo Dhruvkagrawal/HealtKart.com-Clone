0d85c35d010e36ab650a49db0660cc989dc68c9d
srinivas217 <87422030+srinu217@users.noreply.github.com>
2021-12-15 12:57:07 +0530

    address
    address form

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 const express = require("express");
+
+const addressController = require("./controllers/address.controller");
+
+
 const app = express();
+app.use(express.json());
+
+app.use("/users", addressController);
 
 module.exports = app;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
