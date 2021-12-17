33f41103405b040a82c48fa0574e24ce1204e048
arpit1024 <91615308+arpit1024@users.noreply.github.com>
2021-12-17 21:13:44 +0530

    updated files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const path = require('path')
 const { register, login } = require("./controllers/user.controller");
+<<<<<<< Updated upstream
 const flashsale_protiensController = require("./controllers/flashsale_protiens.controller");
 const CartController = require("./controllers/cart.controller");
+=======
+const  flashSaleCont = require("./controllers/flashsale_protiens.controller");
+>>>>>>> Stashed changes
 const passport = require('./configs/passport');
 
 app.use(express.json());
@@ -11,12 +15,17 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.post("/register", register);
 app.post("/login", login);
+<<<<<<< Updated upstream
 app.use("/flashsale", flashsale_protiensController);
 app.use("/cart", CartController);
 
 
 app.set("view engine", "ejs");
 
+=======
+app.use("/flashsale", flashSaleCont)
+app.set('view engine', 'ejs')
+>>>>>>> Stashed changes
 
 app.use(passport.initialize());
 
